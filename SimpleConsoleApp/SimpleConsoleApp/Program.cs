5c936e5d735b8ba321d3aa7a6e208d23ae254fb8
dotnetcustoms <dandreibt@gmail.com>
2014-06-19 00:29:42 +0300

    Initial project structure

---
+++ SimpleConsoleApp/SimpleConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine( "Hello Git!" );
+        }
+    }
+}
